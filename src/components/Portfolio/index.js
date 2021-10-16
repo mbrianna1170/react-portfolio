e7a0ca1c2d0efab7bd0952a9a5fdf202af59d7eb
mbrianna1170 <brianna.miller09@yahoo.com>
2021-10-16 16:44:41 -0500

    fixed divs

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -9,15 +9,24 @@ function Portfolio() {
     <div className="portfolio-wrapper">
       <div className="image-container">
         <h1 className="text-center py-5" id="heading">Portfolio</h1>
+        <div className="figure">
           <img className="portfolio-image" src={freeMaps} alt="" />
-          {/* - */}
+          <p>Free Maps Project</p>
+        </div>
+        <div className="figure">
           <img className="portfolio-image" src={freeMaps} alt="" />
-          {/* - */}
+          <p>Free Maps Project</p>
+        </div>
+        <div className="figure">
           <img className="portfolio-image" src={freeMaps} alt="" />
-          {/* - */}
+          <p>Free Maps Project</p>
+        </div>
+        <div className="figure">
           <img className="portfolio-image" src={freeMaps} alt="" />
+          <p>Free Maps Project</p>
         </div>
       </div>
+    </div>
 
   )
 }
