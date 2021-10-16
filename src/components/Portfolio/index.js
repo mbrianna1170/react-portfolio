247a4decf4487cf7559e757cff573ca47c379e3b
mbrianna1170 <brianna.miller09@yahoo.com>
2021-10-16 17:28:07 -0500

    media queries for portfolio section

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -10,20 +10,33 @@ function Portfolio() {
       <div className="image-container">
         <h1 className="text-center py-5" id="heading">Portfolio</h1>
         <div className="figure">
-          <img className="portfolio-image" src={freeMaps} alt="" />
-          <p>Free Maps Project</p>
+          <img className="portfolio-image" src={freeMaps} alt="Free Maps Project" />
+          <p>MongoDB/Express/React/Node</p>
+          <a href="https://github.com/mbrianna1170/free-maps" target="_blank">Deployed Application</a>
+          <br />
+          <a href="https://github.com/mbrianna1170/free-maps" target="_blank">GitHub Repo</a>
         </div>
         <div className="figure">
           <img className="portfolio-image" src={freeMaps} alt="" />
-          <p>Free Maps Project</p>
+          <p>MongoDB/Express/React/Node</p>
+          <a href="https://github.com/mbrianna1170/free-maps" target="_blank">Deployed Application</a>
+          <br />
+          <a href="https://github.com/mbrianna1170/free-maps" target="_blank">GitHub Repo</a>
         </div>
         <div className="figure">
           <img className="portfolio-image" src={freeMaps} alt="" />
-          <p>Free Maps Project</p>
+          <p>MongoDB/Express/React/Node</p>
+          <a href="https://github.com/mbrianna1170/free-maps" target="_blank">Deployed Application</a>
+          <br />
+          <a href="https://github.com/mbrianna1170/free-maps" target="_blank">GitHub Repo</a>
         </div>
+
         <div className="figure">
           <img className="portfolio-image" src={freeMaps} alt="" />
-          <p>Free Maps Project</p>
+          <p>MongoDB/Express/React/Node</p>
+          <a href="https://github.com/mbrianna1170/free-maps" target="_blank">Deployed Application</a>
+          <br />
+          <a href="https://github.com/mbrianna1170/free-maps" target="_blank">GitHub Repo</a>
         </div>
       </div>
     </div>
