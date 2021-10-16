76a7bfe6782a5b2966d6143f6315e42704ecb6b5
mbrianna1170 <brianna.miller09@yahoo.com>
2021-10-16 17:52:17 -0500

    fixed styling issues

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -11,32 +11,32 @@ function Portfolio() {
         <h1 className="text-center py-5" id="heading">Portfolio</h1>
         <div className="figure">
           <img className="portfolio-image" src={freeMaps} alt="Free Maps Project" />
-          <p>MongoDB/Express/React/Node</p>
+          <p className="portfolio-text">Free Maps: MERN Stack Application<br/>
           <a href="https://github.com/mbrianna1170/free-maps" target="_blank">Deployed Application</a>
           <br />
-          <a href="https://github.com/mbrianna1170/free-maps" target="_blank">GitHub Repo</a>
+          <a href="https://github.com/mbrianna1170/free-maps" target="_blank">GitHub Repo</a></p>
         </div>
         <div className="figure">
-          <img className="portfolio-image" src={freeMaps} alt="" />
-          <p>MongoDB/Express/React/Node</p>
+          <img className="portfolio-image" src={freeMaps} alt="Healthy Craves Project" />
+          <p className="portfolio-text">Healthy Craves: Front-End Application<br/>
           <a href="https://github.com/mbrianna1170/free-maps" target="_blank">Deployed Application</a>
           <br />
-          <a href="https://github.com/mbrianna1170/free-maps" target="_blank">GitHub Repo</a>
+          <a href="https://github.com/mbrianna1170/free-maps" target="_blank">GitHub Repo</a></p>
         </div>
         <div className="figure">
-          <img className="portfolio-image" src={freeMaps} alt="" />
-          <p>MongoDB/Express/React/Node</p>
+          <img className="portfolio-image" src={freeMaps} alt="Libro-Fy Project" />
+          <p className="portfolio-text">Libro-Fy: Back-End Application<br/>
           <a href="https://github.com/mbrianna1170/free-maps" target="_blank">Deployed Application</a>
           <br />
-          <a href="https://github.com/mbrianna1170/free-maps" target="_blank">GitHub Repo</a>
+          <a href="https://github.com/mbrianna1170/free-maps" target="_blank">GitHub Repo</a></p>
         </div>
 
         <div className="figure">
-          <img className="portfolio-image" src={freeMaps} alt="" />
-          <p>MongoDB/Express/React/Node</p>
+          <img className="portfolio-image" src={freeMaps} alt="Basic Portfolio" />
+          <p className="portfolio-text">Simple HTML and CSS Portfolio<br/>
           <a href="https://github.com/mbrianna1170/free-maps" target="_blank">Deployed Application</a>
           <br />
-          <a href="https://github.com/mbrianna1170/free-maps" target="_blank">GitHub Repo</a>
+          <a href="https://github.com/mbrianna1170/free-maps" target="_blank">GitHub Repo</a></p>
         </div>
       </div>
     </div>
